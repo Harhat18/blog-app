2b2ed81b789b7f2976fcee23a09de283aa401a8a
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-12-26 07:26:26 +0300

    add

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,20 +1,13 @@
-import firebase from "firebase/app";
-import "firebase/database";
+import firebase from 'firebase/app';
+import 'firebase/database'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjU3cyThQXxiuQI0-BO-SQkS5KVacebf0",
-  authDomain: "react-blog-app-84344.firebaseapp.com",
-  databaseURL: "https://react-blog-app-84344-default-rtdb.firebaseio.com",
-  projectId: "react-blog-app-84344",
-  storageBucket: "react-blog-app-84344.appspot.com",
-  messagingSenderId: "523792444295",
-  appId: "1:523792444295:web:cf6734111368f451c9c122",
+    
 };
 
 // Initialize Firebase
-
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-export { database as default };
+export {database as default}
