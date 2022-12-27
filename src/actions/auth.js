2f2dcbd232bae5867346b2a1e0a433021a868f9b
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-12-27 21:05:21 +0300

    add

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,9 @@
-import { firebase, googleAuthProvider } from "../firebase/firebaseConfig";
+import {firebase, googleAuthProvider} from '../firebase/firebaseConfig';
 
 export const login = () => {
-  return firebase.auth().signInWithPopup(googleAuthProvider);
-};
+    return firebase.auth().signInWithPopup(googleAuthProvider);
+}
+
+export const logout = () => {
+    return firebase.auth().signOut();
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
-import AppRouter from "./routers/AppRouter";
-import "./App.css";
-import configureStore from "./store/configureStore";
-import { getBlogsFromDatabase } from "./actions/blogs";
-import { firebase } from "./firebase/firebaseConfig";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import AppRouter from './routers/AppRouter';
+import "./App.css"
+import configureStore from './store/configureStore';
+import { getBlogsFromDatabase  } from './actions/blogs'
+import {firebase} from './firebase/firebaseConfig'
 
 const store = configureStore();
 
 const result = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
-);
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("root"));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
 store.dispatch(getBlogsFromDatabase()).then(() => {
-  ReactDOM.render(result, document.getElementById("root"));
-});
+    ReactDOM.render(result, document.getElementById('root'));
+})
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log("login yapılmış");
-  } else {
-    console.log("kullanıcı çıkış yaptı");
-  }
-});
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+        console.log("login yapılmış");
+        console.log(user);
+    } else {
+        console.log("kullanıcı çıkış yapmış");
+    }
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
